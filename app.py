021da9ab40474666b3e47eaf785d1cfc99e3597b
KonstantinaK98 <konakara98@gmail.com>
2021-04-23 23:22:08 +0300

    start navbar

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def home():
    return render_template('base.html')  
+
+@app.route('/temperature')
+def temperature_converter():
+   return render_template('temperature.html')     
 
 if __name__ == '__main__':
    app.run()
