84f6007d67e3a42e40be57aef3f8ed0f774c2dee
KonstantinaK98 <konakara98@gmail.com>
2021-05-31 19:32:59 +0300

    CSS changes and url shortener added

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST','GET'])
 def home():
-   return render_template('base.html')  
+   if request.method == 'POST':
+      if request.form['submit_button'] == 'Temperature':
+         return render_template('temperature.html')
+      elif request.form['submit_button'] == 'Url Shortener':
+         return render_template('urlshortener.html')
+   else:   
+      return render_template('base.html')  
 
 @app.route('/temperature')
 def temperature_converter():
-   return render_template('temperature.html')     
+   return render_template('temperature.html')
+
+@app.route('/urlshortener', methods=['POST','GET'])
+def urlshortener():
+   if request.method == "POST":
+      if request.form['url'] != '':
+         return
+      else:
+         return render_template('urlshortener.html')  
+   else:       
+      return render_template('urlshortener.html')        
 
 if __name__ == '__main__':
    app.run()
